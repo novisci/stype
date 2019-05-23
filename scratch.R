2b80bdb244f6d89e0111c4bea088c48a5f2bac48
bsaul <bradleysaul@gmail.com>
2019-05-23 15:53:57 -0400

    scratch

--- scratch.R
+++ scratch.R
@@ -3,27 +3,34 @@ library(dplyr)
 library(DenverSugar)
 x <- variable(c(TRUE, FALSE, TRUE, TRUE, NA), 
               short_label = "A test variable",
-              descriptor = list(xx = function(x, ...) length(x)))
+              .descriptors = list(xx = function(x, ...) length(x)))
 x@description
+
 x <- variable(c(TRUE, FALSE, TRUE, TRUE, NA), 
               short_label = "A test variable")
+getDescriptors(x@.Data)
 x@description
-descriptor(x)
-describe(x)
 
-x <- variable(c(TRUE, FALSE, TRUE, TRUE, FALSE), short_label = "A test variable")
+x <- variable(c(TRUE, FALSE, TRUE, TRUE, FALSE), 
+              short_label = "A test variable")
+
 describe(x, g = c("A", "A", "A", "B", "B"), w = c(2, 2, 2, 1, 1))
 
 
 x2 <- variable(rnorm(100))
 describe(x2)
 
-x
-z1 <- data.frame(
-  x = variable(rnorm(10), short_label = "A test variable")
-)
-z1$x
 
+
+
+
+ff <- function(x){
+  purrr::map_dfr(x, ~ tibble::as_tibble(.x@description))
+}
+ff(z1)
+
+z1$z@description
+broom::glance(z1)
 data.frame2 <- setClass(
   "data.frame2",
   contains = "data.frame"
@@ -42,4 +49,17 @@ setMethod(
         out
       }
   }
+)
+
+
+nobs <- list(
+  fun     = function(x, g, w, ...) length(x),
+  label   = "Number of observations",
+  printer = function(var, val) sprintf("%s has %s observations", var, val)
+)
+
+mean_sd <- list(
+  fun     = function(x, g, w, ...) list(mean = mean(x), sd = sd(x)),
+  label   = "Number of observations",
+  printer = function(var, val) sprintf("%s (%s)", val[["mean"]], val[["sd"]])
 )

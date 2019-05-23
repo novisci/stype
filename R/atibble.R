5d59549fac580af88741526cff676cbcf74ad7af
bsaul <bradleysaul@gmail.com>
2019-05-23 15:52:11 -0400

    adding methods for atibbles

---
+++ R/atibble.R
@@ -0,0 +1,58 @@
+#------------------------------------------------------------------------------#
+# Defines the following
+#
+# - atibble: a tibble composed entirely of variables
+
+# tbl_df
+# @name tbl_df-class
+# @aliases tbl_df
+# @exportClass tbl_df
+
+#' @export
+#' @importClassesFrom tibble tbl_df
+NULL
+# setOldClass(c("tbl_df", "tbl", "data.frame"), "tbl_df")
+
+
+#' atibble = analysis tibble
+#' 
+#' @slot name a name
+#' @slot label a label
+#' @export atibble
+atibble <- setClass(
+  "atibble",
+  slots = c(
+    "name"      = "character",
+    "label"     = "character"
+    # TODO: what other slots would be useful?
+    # "timestamp" = "character" 
+  ),
+  contains = "tbl_df"
+)
+
+setMethod(
+  f          = "initialize",
+  signature  = "atibble",
+  definition = function(.Object, name, label, ...){
+    
+    vars <- list(...)
+    vars <- purrr::set_names(vars, purrr::map(vars, ~ .x@name))
+
+    # browser()
+    dt <- tibble::tibble(
+      !!! vars,
+      .name_repair = "check_unique"
+    )
+ 
+    callNextMethod(.Object, name = name, label = label, dt)
+  }
+)
+setValidity(
+  Class  = "atibble",
+  method = function(object){
+    assertthat::validate_that(
+      all(purrr::map_lgl(object, ~ methods::is(.x, "variable"))),
+      msg = "All columns of a tibble must be S4 variable class objects."
+    )
+  }
+)

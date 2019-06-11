5dc36505c64c78c5f3dd98ce50a493aea6fb7f94
bsaul <bradleysaul@gmail.com>
2019-06-11 16:32:41 -0400

    updating v_count tests

--- tests/testthat/test_v_count.R
+++ tests/testthat/test_v_count.R
@@ -3,17 +3,17 @@ context("Testing v_count class")
 test_that(
   "v_count class behaves itself",
   {
-    x1 <- count(c(0L, 1L, 2L, NA_integer_))
+    x1 <- v_count(c(0L, 1L, 2L, NA_integer_))
     expect_s3_class(x1, "v_count")
     
-    expect_equivalent(min(x1, na.rm = TRUE), count(0L))
-    expect_equivalent(max(x1, na.rm = TRUE), count(2L))
-    expect_equivalent(min(x1), count(NA_integer_))
-    expect_equivalent(max(x1), count(NA_integer_))
+    expect_equivalent(min(x1, na.rm = TRUE), v_count(0L))
+    expect_equivalent(max(x1, na.rm = TRUE), v_count(2L))
+    expect_equivalent(min(x1), v_count(NA_integer_))
+    expect_equivalent(max(x1), v_count(NA_integer_))
     
-    expect_equivalent(x1[1:3] + count(1L), count(c(1L:3L)))
+    expect_equivalent(x1[1:3] + v_count(1L), v_count(c(1L:3L)))
     
-    expect_equivalent(x1[1:3] + 1L, count(c(1L:3L)))
+    expect_equivalent(x1[1:3] + 1L, v_count(c(1L:3L)))
   }
 )
 
@@ -21,7 +21,7 @@ test_that(
 test_that(
   "v_count math behaves itself",
   {
-    x1 <- count(c(0L, 1L, 2L))
+    x1 <- v_count(c(0L, 1L, 2L))
     expect_equivalent(range(x1), c(0,2))
     expect_equivalent(median(x1), 1)
   }

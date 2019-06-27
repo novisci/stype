59b2bc8f4e976b801ea9b2098a5967b66bc59d40
bsaul <bradleysaul@gmail.com>
2019-06-27 18:23:48 -0400

    huh?

--- R/v_count.R
+++ R/v_count.R
@@ -196,7 +196,9 @@ vec_arith.integer.v_count <- function(op, x, y) {
 
 #' Math
 #' @name vec_math
-#' @inheritParams vctrs::vec_math
+#' @param fun fun
+#' @param x x
+#' @param ... dots
 #' @importFrom vctrs vec_math vec_math_base
 #' @method vec_math v_count
 #' @export
